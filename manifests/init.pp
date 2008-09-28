e4fa745ba0b29989d5b133b5c671657a58f04830
Micah Anderson <micah@riseup.net>
2008-09-28 18:17:31 -0400

    move the dependency on the latest debian-archive-keyring package outside of the etch lsbdistcodename selector as this package should be the latest on every release of debian, not just etch

--- manifests/init.pp
+++ manifests/init.pp
@@ -89,27 +89,30 @@ class apt {
 			alias => apt_updated;
 	}
 
+        ## This package should really always be current
+        package { "debian-archive-keyring":
+          ensure => latest,
+        }
+          
 	case $lsbdistcodename {
 		etch: {
-			## This package should really always be current
-			package {
-				[ "debian-archive-keyring", "debian-backports-keyring" ]:
-					ensure => latest,
-				}
-
-			# This key was downloaded from
-			# http://backports.org/debian/archive.key
-			# and is needed to bootstrap the backports trustpath
-			file { "${apt_base_dir}/backports.org.key":
-				source => "puppet://$servername/apt/backports.org.key",
-				mode => 0444, owner => root, group => root,
-			}
-			exec { "/usr/bin/apt-key add ${apt_base_dir}/backports.org.key && apt-get update":
-				alias => "backports_key",
-				refreshonly => true,
-				subscribe => File["${apt_base_dir}/backports.org.key"],
-				before => [ File[apt_config], Package["debian-backports-keyring"] ]
-			}
+		  package { "debian-backports-keyring":
+		    ensure => latest,
+		  }
+                  
+		  # This key was downloaded from
+		  # http://backports.org/debian/archive.key
+		  # and is needed to bootstrap the backports trustpath
+		  file { "${apt_base_dir}/backports.org.key":
+		    source => "puppet://$servername/apt/backports.org.key",
+		    mode => 0444, owner => root, group => root,
+		  }
+		  exec { "/usr/bin/apt-key add ${apt_base_dir}/backports.org.key && apt-get update":
+		    alias => "backports_key",
+		    refreshonly => true,
+		    subscribe => File["${apt_base_dir}/backports.org.key"],
+		    before => [ File[apt_config], Package["debian-backports-keyring"] ]
+		  }
 		}
 	}
 }
