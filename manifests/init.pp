e0a6bf1b7a480cead5776962c9524d157aa96f1b
Micah Anderson <micah@riseup.net>
2008-09-28 18:00:56 -0400

    Revert "turn preferences into a simple file"
    This reverts commit 0011e70a24ff49169da294b00b6b39b77fb05c86.

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,7 +44,7 @@ class apt {
 	config_file {
 		# this just pins unstable and testing to very low values
 		"/etc/apt/preferences":
-			source => "puppet://$servername/apt/preferences",
+			content => template("apt/preferences.erb"),
 			# use File[apt_config] to reference a completed configuration
 			# See "The Puppet Semaphor" 2007-06-25 on the puppet-users ML
 			alias => apt_config,
