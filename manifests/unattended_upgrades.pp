0daa30f43504d9660eddc24875826cc583166d84
mh <mh@immerda.ch>
2009-10-30 21:15:21 +0100

    fix another typo

--- manifests/unattended_upgrades.pp
+++ manifests/unattended_upgrades.pp
@@ -2,12 +2,11 @@ class apt::unattended_upgrades {
   package{'unattended-upgrades': ensure => present }
 
   config_file {
-      "/etc/apt/apt.conf.d/unattended_upgrades":
-        content => 'APT::Periodic::Update-Package-Lists "1";
+    "/etc/apt/apt.conf.d/unattended_upgrades":
+      content => 'APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Unattended-Upgrade "1";
 ',
-        before => File[apt_config],
-        require => Package['unattended-upgrades'],
-    }
+      before => File[apt_config],
+      require => Package['unattended-upgrades'],
   }
 }
