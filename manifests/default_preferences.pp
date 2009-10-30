b0e0f720d0f0480c96c377bde6f46a2d8820f646
mh <mh@immerda.ch>
2009-10-30 20:20:42 +0100

    add refactored files

---
+++ manifests/default_preferences.pp
@@ -0,0 +1,16 @@
+class apt::default_preferences {
+  config_file {
+    # this just pins unstable and testing to very low values
+    "/etc/apt/preferences":
+      content => template("apt/preferences.erb"),
+      # use File[apt_config] to reference a completed configuration
+      # See "The Puppet Semaphor" 2007-06-25 on the puppet-users ML
+      alias => apt_config,
+      # only update together
+      require => File["/etc/apt/sources.list"];
+    # little default settings which keep the system sane
+    "/etc/apt/apt.conf.d/from_puppet":
+      content => "APT::Get::Show-Upgraded true;\nDSelect::Clean $real_apt_clean;\n",
+      before => File[apt_config];
+  }
+}
---
+++ manifests/default_sources_list.pp
@@ -0,0 +1,10 @@
+class apt::default_sources_list {
+  config_file {
+    # include main, security and backports
+    # additional sources could be included via an array
+    "/etc/apt/sources.list":
+      content => template("apt/sources.list.erb"),
+      require => Exec[assert_lsbdistcodename];
+  }
+}
+
