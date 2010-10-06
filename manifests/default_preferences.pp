ff73987f30e6566b9a89208e6b1ad96724571e0c
intrigeri <intrigeri@boum.org>
2010-10-06 12:30:50 +0200

    Add per-release preferences files.
    The previous default pinning preferences only supported tracking stable.
    Tracking squeeze or sid is now possible.

--- manifests/default_preferences.pp
+++ manifests/default_preferences.pp
@@ -2,7 +2,7 @@ class apt::default_preferences {
   config_file {
     # this just pins unstable and testing to very low values
     "/etc/apt/preferences":
-      content => template("apt/preferences.erb"),
+      content => template("apt/preferences_${codename}.erb"),
       # use File[apt_config] to reference a completed configuration
       # See "The Puppet Semaphor" 2007-06-25 on the puppet-users ML
       alias => apt_config,
