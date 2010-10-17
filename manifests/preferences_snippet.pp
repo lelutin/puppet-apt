822d482f4d6e94546cb5454d571b4d58525af94b
mh <mh@immerda.ch>
2010-10-17 14:13:02 +0200

    improve prefrences snippet

--- manifests/preferences_snippet.pp
+++ manifests/preferences_snippet.pp
@@ -1,10 +1,14 @@
 define apt::preferences_snippet(
-  $ensure => 'present',
-  $content
+  $ensure = 'present',
+  $release,
+  $priority
 ){
   file { "/var/lib/puppet/modules/apt/preferences/${name}":
     ensure => $ensure,
-    content => "${content}\n",
+    content => "Package: ${name}
+Pin: release a=${release}
+Pin-Priority: ${priority}
+",
     notify => Exec['concat_/var/lib/puppet/modules/apt/preferences'],
     owner => root, group => 0, mode => 0600;
   }
