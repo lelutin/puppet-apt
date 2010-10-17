68842b2f968a6da6a9b654f5da8d061f429d991b
mh <mh@immerda.ch>
2010-10-17 14:36:28 +0200

    fix header file

--- manifests/preferences.pp
+++ manifests/preferences.pp
@@ -3,9 +3,8 @@ class apt::preferences {
   include common::moduledir
   $apt_preferences_dir = "${common::moduledir::module_dir_path}/apt/preferences"
   module_dir{'apt/preferences': }
-  concatenated_file{'/etc/apt/preferences':
-    dir => $apt_preferences_dir,
-    header => 'Package: *
+  file{"${apt_preferences_dir}_header":
+    content => 'Package: *
 Pin: release a=unstable
 Pin-Priority: 1
 
@@ -13,6 +12,11 @@ Package: *
 Pin: release a=testing
 Pin-Priority: 2
 ',
+  }
+
+  concatenated_file{'/etc/apt/preferences':
+    dir => $apt_preferences_dir,
+    header => "${apt_preferences_dir}_header",
     # use Concatenated_file[apt_config] to reference a completed configuration
     # See "The Puppet Semaphor" 2007-06-25 on the puppet-users ML
     alias => apt_config,
