c43bfac6b80489cf789fe448abb3561a6655a969
intrigeri <intrigeri@boum.org>
2010-10-17 17:44:19 +0200

    Ensure apt-listbugs is absent in dist-upgrade mode.
    Else it will crash automatic upgrades.

--- manifests/cron/dist-upgrade.pp
+++ manifests/cron/dist-upgrade.pp
@@ -8,6 +8,8 @@ dist-upgrade -y -o APT::Get::Show-Upgraded=true
     ensure => absent,
   }
 
+  package { "apt-listbugs": ensure => absent }
+
   config_file { "/etc/cron-apt/action.d/4-dist-upgrade":
     content => $action,
     require => Package[cron-apt]
