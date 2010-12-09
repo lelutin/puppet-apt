fbb291ed19eeb9a64d4aed78c48d221315da7866
Micah Anderson <micah@riseup.net>
2010-12-08 22:24:24 -0500

    add apticron support.
    Why apticron, when we have cron-apt already? Some people have different preferences, we use apticron along with the upgrade_package functionality in this module. I know someone who uses cron-apt to run the upgrades, but apticron for notifications, because apticron's notifications are much nicer (cron-apt just gives you the output of apt-get upgrade)

---
+++ manifests/apticron.pp
@@ -0,0 +1,54 @@
+class apt::apticron {
+
+  case $apticron_ensure_version {
+    '': { $apticron_ensure_version = "present" }
+  }
+
+  case $apticron_config {
+    '': { $apticron_config = "apt/${operatingsystem}/apticron_${lsbrelease}.erb" }
+  }
+
+  case $apticron_email {
+    '': { $apticron_email = "root" }
+  }
+
+  case $apticron_diff_only {
+    '': { $apticron_diff_only = "1" }
+  }
+
+  case $apticron_listchanges_profile {
+    '': { $apticron_listchanges_profile = "apticron" }
+  }
+
+  case $apticron_system {
+    '': { $apticron_system = false }
+  }
+
+  case $apticron_ipaddressnum {
+    '': { $apticron_ipaddressnum = false }
+  }
+
+  case $apticron_ipaddresses {
+    '': { $apticron_ipaddresses = false }
+  }
+
+  case $apticron_notifyholds {
+    '': { $apticron_notifyholds = "0" }
+  }
+
+  case $apticron_notifynew {
+    '': { $apticron_notifynew = "0" }
+  }
+
+  case $apticron_customsubject {
+    '': { $apticron_customsubject = "" }
+  }
+  
+  package { apticron: ensure => $apticron_ensure_version }
+
+  file { "/etc/apticron/apticron.conf":
+    content => template($apticron_config),
+    mode => 0644, owner => root, group => root,
+    require => Package["apticron"];  
+  }
+}
