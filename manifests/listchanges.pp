2d4f6d3522e8ef157763b2788de6dc71dc054be2
intrigeri <intrigeri@boum.org>
2010-12-15 22:11:34 +0100

    Fix 5237f9da.

--- manifests/listchanges.pp
+++ manifests/listchanges.pp
@@ -1,31 +1,31 @@
 class apt::listchanges {
 
   case $apt_listchanges_version {
-    '': { $listchanges_version = "present" }
+    '': { $apt_listchanges_version = "present" }
   }
 
   case $apt_listchanges_config {
-    '': { $listchanges_config = "apt/${operatingsystem}/listchanges_${lsbdistcodename}.erb" }
+    '': { $apt_listchanges_config = "apt/${operatingsystem}/listchanges_${lsbdistcodename}.erb" }
   }
 
   case $apt_listchanges_frontend {
-    '': { $listchanges_frontend = "mail" }
+    '': { $apt_listchanges_frontend = "mail" }
   }
 
   case $apt_listchanges_email {
-    '': { $listchanges_email = "root" }
+    '': { $apt_listchanges_email = "root" }
   }
 
   case $apt_listchanges_confirm {
-    '': { $listchanges_confirm = "0" }
+    '': { $apt_listchanges_confirm = "0" }
   }
 
   case $apt_listchanges_saveseen {
-    '': { $listchanges_saveseen = "/var/lib/apt/listchanges.db" }
+    '': { $apt_listchanges_saveseen = "/var/lib/apt/listchanges.db" }
   }
 
   case $apt_listchanges_which {
-    '': { $listchanges_which = "both" }
+    '': { $apt_listchanges_which = "both" }
   }
 
   package { apt-listchanges: ensure => $apt_listchanges_ensure_version }
