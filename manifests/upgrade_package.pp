4204847fdd423dfaff0dafb5130080a30003921f
nadir <nadir-technik@nadir.org>
2012-11-08 10:07:38 +0100

    added $apt_disable_update to disable "apt-get update" during puppetruns

--- manifests/upgrade_package.pp
+++ manifests/upgrade_package.pp
@@ -1,6 +1,8 @@
 define apt::upgrade_package ($version = "") {
 
-  include apt::update
+  if $apt::disable_update == false { 
+    include apt::update 
+  }
 
   $version_suffix = $version ? {
     ''       => '',
@@ -24,10 +26,11 @@ define apt::upgrade_package ($version = "") {
 
   exec { "apt-get -q -y -o 'DPkg::Options::=--force-confold' install ${name}${version_suffix}":
     onlyif => [ "grep-status -F Status installed -a -P $name -q", "apt-show-versions -u $name | grep -q upgradeable" ],
-    require => [
-      Exec['apt_updated'],
-      Package['apt-show-versions', 'dctrl-tools'],
-    ],
+    require => $apt::disable_update ? {
+      true    => Package['apt-show-versions', 'dctrl-tools'],
+      default => [ Exec['apt_updated'], 
+                 Package['apt-show-versions', 'dctrl-tools'] ],
+    } 
   }
 
 }
