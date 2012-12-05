lelutin/puppet-apt

commit ac166366d7baa45b74e09974289d9fb2dad38e67

refactor things for >2.7

manifests/init.pp
@@ -6,7 +6,7 @@
 class apt(
   $use_volatile = hiera('apt_volatile_enabled',false),
   $include_src = hiera('apt_include_src',false),
-  $use_next_release = hiera('apt_use_next_release'),
+  $use_next_release = hiera('apt_use_next_release',false),
   $debian_url = hiera('apt_debian_url','http://cdn.debian.net/debian/'),
   $security_url = hiera('apt_security_url','http://security.debian.org/'),
   $backports_url = hiera('apt_backports_url','http://backports.debian.org/debian-backports/'),
@@ -40,16 +40,14 @@ class apt(
   # init $release, $next_release, $codename, $next_codename, $release_version
   case $::lsbdistcodename {
     '': {
-      $codename = $::lsbdistcodename
       $release = $::lsbdistrelease
     }
     default: {
-      $codename = $::lsbdistcodename
-      $release = debian_release($codename)
+      $release = debian_release($::lsbdistcodename)
     }
   }
-  $release_version = debian_release_version($codename)
-  $next_codename = debian_nextcodename($codename)
+  $release_version = debian_release_version($::lsbdistcodename)
+  $next_codename = debian_nextcodename($::lsbdistcodename)
   $next_release = debian_nextrelease($release)
 
   config_file {

manifests/preferences.pp
@@ -3,8 +3,8 @@ class apt::preferences {
   concat::fragment{"apt_preferences_header":
     content => $apt::custom_preferences ? {
       '' => $::operatingsystem ? {
-        'debian' => template("apt/${::operatingsystem}/preferences_${::codename}.erb"),
-        'ubuntu' => template("apt/${::operatingsystem}/preferences_${::codename}.erb"),
+        'debian' => template("apt/${::operatingsystem}/preferences_${::lsbdistcodename}.erb"),
+        'ubuntu' => template("apt/${::operatingsystem}/preferences_${::lsbdistcodename}.erb"),
       },
       default => $custom_preferences
     },

templates/Debian/preferences_lenny.erb
@@ -1,6 +1,6 @@
-Explanation: Debian <%= codename %>
+Explanation: Debian <%= scope.lookupvar('::lsbdistcodename') %>
 Package: *
-Pin: release o=Debian,a=<%= release %>,v=<%= release_version %>*
+Pin: release o=Debian,a=<%= scope.lookupvar('apt::release') %>,v=<%= scope.lookupvar('apt::release_version') %>*
 Pin-Priority: 990
 
 Explanation: Debian backports
@@ -8,9 +8,9 @@ Package: *
 Pin: origin backports.debian.org
 Pin-Priority: 200
 
-Explanation: Debian <%= next_release %>
+Explanation: Debian <%= scope.lookupvar('apt::next_release') %>
 Package: *
-Pin: release o=Debian,a=<%= next_release %>
+Pin: release o=Debian,a=<%= scope.lookupvar('apt::next_release') %>
 Pin-Priority: 2
 
 Explanation: Debian sid

templates/Debian/preferences_squeeze.erb
@@ -1,16 +1,16 @@
-Explanation: Debian <%= codename %>
+Explanation: Debian <%= scope.lookupvar('::lsbdistcodename') %>
 Package: *
-Pin: release o=Debian,n=<%= codename %>
+Pin: release o=Debian,n=<%= scope.lookupvar('::lsbdistcodename') %>
 Pin-Priority: 990
 
-Explanation: Debian <%= codename %>-updates
+Explanation: Debian <%= scope.lookupvar('::lsbdistcodename') %>-updates
 Package: *
-Pin: release o=Debian,n=<%= codename %>-updates
+Pin: release o=Debian,n=<%= scope.lookupvar('::lsbdistcodename') %>-updates
 Pin-Priority: 990
 
-Explanation: Debian <%= next_codename %>
+Explanation: Debian <%= scope.lookupvar('apt::next_codename') %>
 Package: *
-Pin: release o=Debian,n=<%= next_codename %>
+Pin: release o=Debian,n=<%= scope.lookupvar('apt::next_codename') %>
 Pin-Priority: 2
 
 Explanation: Debian sid

templates/Debian/preferences_wheezy.erb
@@ -1,11 +1,11 @@
-Explanation: Debian <%= codename %>
+Explanation: Debian <%= scope.lookupvar('::lsbdistcodename') %>
 Package: *
-Pin: release o=Debian,n=<%= codename %>
+Pin: release o=Debian,n=<%= scope.lookupvar('::lsbdistcodename') %>
 Pin-Priority: 990
 
-Explanation: Debian <%= codename %>-updates
+Explanation: Debian <%= scope.lookupvar('::lsbdistcodename') %>-updates
 Package: *
-Pin: release o=Debian,n=<%= codename %>-updates
+Pin: release o=Debian,n=<%= scope.lookupvar('::lsbdistcodename') %>-updates
 Pin-Priority: 990
 
 Explanation: Debian sid

templates/Debian/sources.list.erb
@@ -1,20 +1,20 @@
 # This file is managed by puppet
 # all local modifications will be overwritten
 
-### Debian current: <%= codename %>
+### Debian current: <%= codename = scope.lookupvar('::lsbdistcodename') %>
 
 # basic
-deb <%= debian_url %> <%= codename %> <%= repos %>
-<% if include_src then -%>
+deb <%= debian_url=scope.lookupvar('apt::debian_url') %> <%= codename %> <%= repos=scope.lookupvar('apt::repos') %>
+<% if scope.lookupvar('apt::include_src')  -%>
 deb-src <%= debian_url %> <%= codename %> <%= repos %>
 <% end -%>
 
 # security
-<% if (release == "unstable" || release == "experimental") -%>
+<% if ((release=scope.lookupvar('apt::release')) == "unstable" || release == "experimental") -%>
 # There is no security support for <%= release %>
 <% else -%>
-deb <%= security_url %> <%= codename %>/updates <%= repos %>
-<% if include_src then -%>
+deb <%= security_url=scope.lookupvar('apt::security_url') %> <%= codename %>/updates <%= repos %>
+<% if include_src -%>
 deb-src <%= security_url %> <%= codename %>/updates <%= repos %>
 <% end -%>
 <% end -%>
@@ -23,33 +23,33 @@ deb-src <%= security_url %> <%= codename %>/updates <%= repos %>
 <% if (release == "testing" || release == "unstable" || release == "experimental") -%>
 # There is no backports archive for <%= release %>
 <% else -%>
-deb <%= backports_url %> <%= codename %>-backports <%= repos %>
-<% if include_src then -%>
+deb <%= backports_url=scope.lookupvar('apt::backports_url') %> <%= codename %>-backports <%= repos %>
+<% if include_src -%>
 deb-src <%= backports_url %> <%= codename %>-backports <%= repos %>
 <% end -%>
 <% end -%>
 
-<% if use_volatile -%>
+<% if use_volatile=scope.lookupvar('apt::use_volatile') -%>
 # volatile
 <% if (release == "testing" || release == "unstable" || release == "experimental") -%>
 # There is no volatile archive for <%= release %>
 <% else -%>
 <% if (codename == "lenny" || codename == "etch") -%>
-deb <%= volatile_url %> <%= codename %>/volatile <%= repos %>
-<% if include_src then -%>
+deb <%= volatile_url=scope.lookupvar('apt::volatile_url') %> <%= codename %>/volatile <%= repos %>
+<% if include_src -%>
 deb-src <%= volatile_url %> <%= codename %>/volatile <%= repos %>
 <% end -%>
 <% else -%>
 deb <%= debian_url %> <%= codename %>-updates <%= repos %>
-<% if include_src then -%>
+<% if include_src -%>
 deb-src <%= debian_url %> <%= codename %>-updates <%= repos %>
 <% end -%>
 <% end -%>
 <% end -%>
 <% end -%>
 
-<% if use_next_release then -%>
-### Debian next: <%= next_codename %>
+<% if next_release=scope.lookupvar('apt::use_next_release') -%>
+### Debian next: <%= next_codename=scope.lookupvar('apt::next_codename') %>
 
 # basic
 deb <%= debian_url %> <%= next_codename %> <%= repos %>

templates/Ubuntu/preferences_maverick.erb
@@ -1,26 +1,26 @@
-Explanation: Ubuntu <%= codename %> security 
+Explanation: Ubuntu <%= scope.lookupvar('::lsbdistcodename') %> security
 Package: *
-Pin: release o=Ubuntu,a=<%= codename %>-security
+Pin: release o=Ubuntu,a=<%= scope.lookupvar('::lsbdistcodename') %>-security
 Pin-Priority: 990
 
-Explanation: Ubuntu <%= codename %> updates
+Explanation: Ubuntu <%= scope.lookupvar('::lsbdistcodename') %> updates
 Package: *
-Pin: release o=Ubuntu,a=<%= codename %>-updates
+Pin: release o=Ubuntu,a=<%= scope.lookupvar('::lsbdistcodename') %>-updates
 Pin-Priority: 980
 
-Explanation: Ubuntu <%= codename %>
+Explanation: Ubuntu <%= scope.lookupvar('::lsbdistcodename') %>
 Package: *
-Pin: release o=Ubuntu,a=<%= codename %>
+Pin: release o=Ubuntu,a=<%= scope.lookupvar('::lsbdistcodename') %>
 Pin-Priority: 970
 
 Explanation: Ubuntu backports
 Package: *
-Pin: release a=<%= codename %>-backports
+Pin: release a=<%= scope.lookupvar('::lsbdistcodename') %>-backports
 Pin-Priority: 200
 
-Explanation: Ubuntu <%= next_release %>
+Explanation: Ubuntu <%= scope.lookupvar('apt::next_release') %>
 Package: *
-Pin: release o=Ubuntu,a=<%= next_release %>
+Pin: release o=Ubuntu,a=<%= scope.lookupvar('apt::next_release') %>
 Pin-Priority: 2
 
 Explanation: Ubuntu fallback

templates/Ubuntu/sources.list.erb
@@ -1,26 +1,26 @@
 # This file is managed by puppet
 # all local modifications will be overwritten
 
-# basic <%= lsbdistcodename %>
-deb <%= ubuntu_url %> <%= lsbdistcodename %> <%= repos %>
-<% if include_src then -%>
+# basic <%= lsbdistcodename=scope.lookupvar('::lsbdistcodename') %>
+deb <%= ubuntu_url=scope.lookupvar('apt::ubuntu_url') %> <%= lsbdistcodename %> <%= repos=scope.lookupvar('apt::repos') %>
+<% if include_src=scope.lookupvar('apt::include_src') -%>
 deb-src <%= ubuntu_url %> <%= lsbdistcodename %> <%= repos %>
 <% end -%>
 
 # updates
 deb <%= ubuntu_url %> <%= lsbdistcodename %>-updates <%= repos %>
-<% if include_src then -%>
+<% if include_src -%>
 deb-src <%= ubuntu_url %> <%= lsbdistcodename %>-updates <%= repos %>
 <% end -%>
 
 # security suppport
 deb <%= ubuntu_url %> <%= lsbdistcodename %>-security <%= repos %>
-<% if include_src then -%>
+<% if include_src -%>
 deb-src <%= ubuntu_url %> <%= lsbdistcodename %>-security <%= repos %>
 <% end -%>
 
 # backports
 deb <%= ubuntu_url %> <%= lsbdistcodename %>-backports main <%= repos %>
-<% if include_src then -%>
+<% if include_src -%>
 deb-src <%= ubuntu_url %> <%= lsbdistcodename %>-backports <%= repos %>
 <% end -%>

commit 07a031f2050deecfefa205e1e0c63586692a06ff

fix for $lsbdistcodename regression introduced by ac166366d7baa45b74e09974289d9fb2dad38e67
What happened here was the $codename = $::lsbdistcodename was removed from
init.pp and replaced with just $::lsbdistcodename whereever $codename was
used. Then in the sources.list.erb and preferences files things were changed
like this:

<pre>+### Debian current: <%= codename = scope.lookupvar('::lsbdistcodename') %>
...
-deb <%= debian_url %> <%= codename %> <%= repos %>
...
+deb <%= debian_url=scope.lookupvar('apt::debian_url') %> <%= codename %> <%= repos=scope.lookupvar('apt::repos') %>
</pre>

This meant that the codename was always set to lsbdistcodename, and you because
in newer puppet versions you cannot assign a value to a top-level facter
variable, it is not possible to change this.

Because we cannot change $lsbdistcodename, we have to fix this by allowing the
user to pass a different, non-top-level scoped variable to the class as a
parameter, which defaults to $::lsbdistcodename, so that upgrades can be
triggered.

This is documented in the README in an upgrade notice

README
@@ -31,6 +31,17 @@ Ubuntu support is lagging behind but not absent either.
 
     class { 'apt': debian_url => "http://localhost:9999/debian/", use_next_release => true }
 
+   previously, you could manually set $lsbdistcodename which would enable forced
+   upgrades, but because this is a top-level facter variable, and newer puppet
+   versions do not let you assign variables to other namespaces, this is no
+   longer possible. However, there is a way to obtain this functionality, and
+   that is to pass the 'codename' parameter to the apt class, which will change
+   the sources.list and preferences files to be the codename you set, allowing
+   you to trigger upgrades:
+
+   include apt::dist_upgrade
+   class { 'apt': codename => 'wheezy', notify => Exec['apt_dist-upgrade'] }
+
  * the apticron class has been moved to a parameterized class.  if you were
    including this class before, you will need to move to instantiating the
    class instead. For example, if you had the following in your manifests:
@@ -105,13 +116,6 @@ site_apt/files/some.host.com/03clean_vserver)
 
 Variables
 =========
-
-$::lsbdistcodename
-----------------
-
-Contains the codename ("etch", "lenny", ...) of the client's
-release. While these values come from lsb-release by default, this
-value can be set manually too, e.g. to enable forced upgrades.
 
 $custom_sources_list
 --------------------
@@ -218,6 +222,16 @@ Setting this variable to false before including this class will force the
 apt/preferences file to be absent:
 
   class { 'apt': custom_preferences => false }
+
+codename
+--------
+
+Contains the codename ("squeeze", "wheezy", ...) of the client's release. While
+these values come from lsb-release by default, this parameter can be set
+manually, e.g. to enable forced upgrades. For example:
+
+   include apt::dist_upgrade
+   class { 'apt': codename => 'wheezy', notify => Exec['apt_dist-upgrade'] }
 
 
 apt::apticron

manifests/init.pp
@@ -4,6 +4,7 @@
 # See LICENSE for the full license granted to you.
 
 class apt(
+  $codename = '',
   $use_volatile = false,
   $include_src = false,
   $use_next_release = false,
@@ -38,16 +39,17 @@ class apt(
   include lsb
 
   # init $release, $next_release, $codename, $next_codename, $release_version
-  case $::lsbdistcodename {
+  case $codename {
     '': {
+      $codename = $::lsbdistcodename
       $release = $::lsbdistrelease
     }
     default: {
-      $release = debian_release($::lsbdistcodename)
+      $release = debian_release($codename)
     }
   }
-  $release_version = debian_release_version($::lsbdistcodename)
-  $next_codename = debian_nextcodename($::lsbdistcodename)
+  $release_version = debian_release_version($codename)
+  $next_codename = debian_nextcodename($codename)
   $next_release = debian_nextrelease($release)
 
   file {

manifests/preferences.pp
@@ -3,8 +3,8 @@ class apt::preferences {
   concat::fragment{"apt_preferences_header":
     content => $apt::custom_preferences ? {
       '' => $::operatingsystem ? {
-        'debian' => template("apt/${::operatingsystem}/preferences_${::lsbdistcodename}.erb"),
-        'ubuntu' => template("apt/${::operatingsystem}/preferences_${::lsbdistcodename}.erb"),
+        'debian' => template("apt/${::operatingsystem}/preferences_${apt::codename}.erb"),
+        'ubuntu' => template("apt/${::operatingsystem}/preferences_${apt::codename}.erb"),
       },
       default => $custom_preferences
     },

templates/Debian/preferences_lenny.erb
@@ -1,4 +1,4 @@
-Explanation: Debian <%= scope.lookupvar('::lsbdistcodename') %>
+Explanation: Debian <%= codename=scope.lookupvar('apt::codename') %>
 Package: *
 Pin: release o=Debian,a=<%= scope.lookupvar('apt::release') %>,v=<%= scope.lookupvar('apt::release_version') %>*
 Pin-Priority: 990
@@ -8,9 +8,9 @@ Package: *
 Pin: origin backports.debian.org
 Pin-Priority: 200
 
-Explanation: Debian <%= scope.lookupvar('apt::next_release') %>
+Explanation: Debian <%= next_release=scope.lookupvar('apt::next_release') %>
 Package: *
-Pin: release o=Debian,a=<%= scope.lookupvar('apt::next_release') %>
+Pin: release o=Debian,a=<%= next_release %>
 Pin-Priority: 2
 
 Explanation: Debian sid

templates/Debian/preferences_squeeze.erb
@@ -1,16 +1,16 @@
-Explanation: Debian <%= scope.lookupvar('::lsbdistcodename') %>
+Explanation: Debian <%= codename=scope.lookupvar('apt::codename') %>
 Package: *
-Pin: release o=Debian,n=<%= scope.lookupvar('::lsbdistcodename') %>
+Pin: release o=Debian,n=<%= codename %>
 Pin-Priority: 990
 
-Explanation: Debian <%= scope.lookupvar('::lsbdistcodename') %>-updates
+Explanation: Debian <%= codename %>-updates
 Package: *
-Pin: release o=Debian,n=<%= scope.lookupvar('::lsbdistcodename') %>-updates
+Pin: release o=Debian,n=<%= codename %>-updates
 Pin-Priority: 990
 
-Explanation: Debian <%= scope.lookupvar('apt::next_codename') %>
+Explanation: Debian <%= next_codename=scope.lookupvar('apt::next_codename') %>
 Package: *
-Pin: release o=Debian,n=<%= scope.lookupvar('apt::next_codename') %>
+Pin: release o=Debian,n=<%= next_codename %>
 Pin-Priority: 2
 
 Explanation: Debian sid

templates/Debian/preferences_wheezy.erb
@@ -1,11 +1,11 @@
-Explanation: Debian <%= scope.lookupvar('::lsbdistcodename') %>
+Explanation: Debian <%= codename=scope.lookupvar('apt::codename') %>
 Package: *
-Pin: release o=Debian,n=<%= scope.lookupvar('::lsbdistcodename') %>
+Pin: release o=Debian,n=<%= codename %>
 Pin-Priority: 990
 
-Explanation: Debian <%= scope.lookupvar('::lsbdistcodename') %>-updates
+Explanation: Debian <%= codename %>-updates
 Package: *
-Pin: release o=Debian,n=<%= scope.lookupvar('::lsbdistcodename') %>-updates
+Pin: release o=Debian,n=<%= codename %>-updates
 Pin-Priority: 990
 
 Explanation: Debian sid

templates/Debian/sources.list.erb
@@ -1,7 +1,7 @@
 # This file is managed by puppet
 # all local modifications will be overwritten
 
-### Debian current: <%= codename = scope.lookupvar('::lsbdistcodename') %>
+### Debian current: <%= codename=scope.lookupvar('apt::codename') %>
 
 # basic
 deb <%= debian_url=scope.lookupvar('apt::debian_url') %> <%= codename %> <%= lrepos=scope.lookupvar('apt::real_repos') %>

templates/Ubuntu/preferences_maverick.erb
@@ -1,26 +1,26 @@
-Explanation: Ubuntu <%= scope.lookupvar('::lsbdistcodename') %> security
+Explanation: Ubuntu <%= codename=scope.lookupvar('apt::codename') %> security
 Package: *
-Pin: release o=Ubuntu,a=<%= scope.lookupvar('::lsbdistcodename') %>-security
+Pin: release o=Ubuntu,a=<%= codename %>-security
 Pin-Priority: 990
 
-Explanation: Ubuntu <%= scope.lookupvar('::lsbdistcodename') %> updates
+Explanation: Ubuntu <%= codename %> updates
 Package: *
-Pin: release o=Ubuntu,a=<%= scope.lookupvar('::lsbdistcodename') %>-updates
+Pin: release o=Ubuntu,a=<%= codename %>-updates
 Pin-Priority: 980
 
-Explanation: Ubuntu <%= scope.lookupvar('::lsbdistcodename') %>
+Explanation: Ubuntu <%= codename %>
 Package: *
-Pin: release o=Ubuntu,a=<%= scope.lookupvar('::lsbdistcodename') %>
+Pin: release o=Ubuntu,a=<%= codename %>
 Pin-Priority: 970
 
 Explanation: Ubuntu backports
 Package: *
-Pin: release a=<%= scope.lookupvar('::lsbdistcodename') %>-backports
+Pin: release a=<%= codename %>-backports
 Pin-Priority: 200
 
-Explanation: Ubuntu <%= scope.lookupvar('apt::next_release') %>
+Explanation: Ubuntu <%= next_release=scope.lookupvar('apt::next_release') %>
 Package: *
-Pin: release o=Ubuntu,a=<%= scope.lookupvar('apt::next_release') %>
+Pin: release o=Ubuntu,a=<%= next_release %>
 Pin-Priority: 2
 
 Explanation: Ubuntu fallback

templates/Ubuntu/sources.list.erb
@@ -1,26 +1,26 @@
 # This file is managed by puppet
 # all local modifications will be overwritten
 
-# basic <%= lsbdistcodename=scope.lookupvar('::lsbdistcodename') %>
-deb <%= ubuntu_url=scope.lookupvar('apt::ubuntu_url') %> <%= lsbdistcodename %> <%= lrepos=scope.lookupvar('apt::real_repos') %>
+# basic <%= codename=scope.lookupvar('apt::codename') %>
+deb <%= ubuntu_url=scope.lookupvar('apt::ubuntu_url') %> <%= codename %> <%= lrepos=scope.lookupvar('apt::real_repos') %>
 <% if include_src=scope.lookupvar('apt::include_src') -%>
-deb-src <%= ubuntu_url %> <%= lsbdistcodename %> <%= lrepos %>
+deb-src <%= ubuntu_url %> <%= codename %> <%= lrepos %>
 <% end -%>
 
 # updates
-deb <%= ubuntu_url %> <%= lsbdistcodename %>-updates <%= lrepos %>
+deb <%= ubuntu_url %> <%= codename %>-updates <%= lrepos %>
 <% if include_src -%>
-deb-src <%= ubuntu_url %> <%= lsbdistcodename %>-updates <%= lrepos %>
+deb-src <%= ubuntu_url %> <%= codename %>-updates <%= lrepos %>
 <% end -%>
 
 # security suppport
-deb <%= ubuntu_url %> <%= lsbdistcodename %>-security <%= lrepos %>
+deb <%= ubuntu_url %> <%= codename %>-security <%= lrepos %>
 <% if include_src -%>
-deb-src <%= ubuntu_url %> <%= lsbdistcodename %>-security <%= lrepos %>
+deb-src <%= ubuntu_url %> <%= codename %>-security <%= lrepos %>
 <% end -%>
 
 # backports
-deb <%= ubuntu_url %> <%= lsbdistcodename %>-backports main <%= lrepos %>
+deb <%= ubuntu_url %> <%= codename %>-backports main <%= lrepos %>
 <% if include_src -%>
-deb-src <%= ubuntu_url %> <%= lsbdistcodename %>-backports <%= lrepos %>
+deb-src <%= ubuntu_url %> <%= codename %>-backports <%= lrepos %>
 <% end -%>