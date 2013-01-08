14670466be5eceba043e697d5f6f0f0ada9d5b07
intrigeri <intrigeri@boum.org>
2013-01-08 18:13:23 +0100

    Don't check for source / content to be set when removing an APT source.
    Else, the sanity checks prevent one from using a simple
      apt::sources_list { "sid.list": ensure => absent }

--- manifests/sources_list.pp
+++ manifests/sources_list.pp
@@ -4,11 +4,13 @@ define apt::sources_list (
   $content = undef
 ) {
 
-  if $source == '' and $content == undef {
-    fail("One of \$source or \$content must be specified for apt_sources_snippet ${name}")
-  }
-  if $source != '' and $content != undef {
-    fail("Only one of \$source or \$content must specified for apt_sources_snippet ${name}")
+  if $ensure == 'present' {
+    if $source == '' and $content == undef {
+      fail("One of \$source or \$content must be specified for apt_sources_snippet ${name}")
+    }
+    if $source != '' and $content != undef {
+      fail("Only one of \$source or \$content must specified for apt_sources_snippet ${name}")
+    }
   }
 
   include apt::dot_d_directories
