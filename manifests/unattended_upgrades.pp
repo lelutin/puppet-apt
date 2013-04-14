9a3068016feffa328bf6e0f265e316eb813b7ee8
varac <varacanero@zeromail.org>
2013-04-14 14:54:10 +0200

    using distribution sprecific files again

--- manifests/unattended_upgrades.pp
+++ manifests/unattended_upgrades.pp
@@ -7,10 +7,10 @@ class apt::unattended_upgrades {
 
   apt_conf { '50unattended-upgrades':
     source  => [
-                "puppet:///modules/site_apt/${::lsbdistcodename}/50unattended-upgrades",
-                'puppet:///modules/site_apt/50unattended-upgrades',
-                "puppet:///modules/apt/${::lsbdistcodename}/50unattended-upgrades",
-                'puppet:///modules/apt/50unattended-upgrades' ],
+                "puppet:///modules/site_apt/${::lsbdistid}/50unattended-upgrades.${::lsbdistcodename}",
+                "puppet:///modules/site_apt/${::lsbdistid}/50unattended-upgrades",
+                "puppet:///modules/apt/${::lsbdistid}/50unattended-upgrades.${::lsbdistcodename}",
+                "puppet:///modules/apt/${::lsbdistid}/50unattended-upgrades" ],
     require => Package['unattended-upgrades'],
   }
 
