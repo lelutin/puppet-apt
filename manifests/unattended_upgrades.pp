9192785c452ce2cfa0f58984d3aebdcaf841113a
Gabriel Filion <gabster@lelutin.ca>
2013-07-27 13:38:27 -0400

    make it possible to opt out of MailOnlyOnError
    This combines all files into one template. It should make maintenance
    easier.

--- manifests/unattended_upgrades.pp
+++ manifests/unattended_upgrades.pp
@@ -1,15 +1,19 @@
-class apt::unattended_upgrades {
+class apt::unattended_upgrades (
+  $config_content = undef,
+  $mailonlyonerror = true,
+) {
 
   package { 'unattended-upgrades':
     ensure  => present
   }
 
+  $file_content = $config_content ? {
+    undef   => template('apt/50unattended-upgrades.erb'),
+    default => $config_content
+  }
+
   apt_conf { '50unattended-upgrades':
-    source  => [
-                "puppet:///modules/site_apt/${::lsbdistid}/50unattended-upgrades.${::lsbdistcodename}",
-                "puppet:///modules/site_apt/${::lsbdistid}/50unattended-upgrades",
-                "puppet:///modules/apt/${::lsbdistid}/50unattended-upgrades.${::lsbdistcodename}",
-                "puppet:///modules/apt/${::lsbdistid}/50unattended-upgrades" ],
+    content => $file_content,
     require => Package['unattended-upgrades'],
   }
 }
