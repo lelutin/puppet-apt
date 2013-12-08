f8aa2ef8722592b61fcbc10959c571ff19dee573
intrigeri <intrigeri@boum.org>
2013-12-08 21:58:20 +0000

    Drop Debian Etch and Lenny compatibility.
    Both have been unsupported for ages.

--- lib/puppet/parser/functions/debian_release_version.rb
+++ lib/puppet/parser/functions/debian_release_version.rb
@@ -1,8 +1,6 @@
 module Puppet::Parser::Functions
   newfunction(:debian_release_version, :type => :rvalue) do |args|
     case args[0]
-      when 'etch'         then '4.0'
-      when 'lenny'        then '5.0'
       when 'squeeze'      then '6.0'
       when 'wheezy'       then '7.0'
       else ''
