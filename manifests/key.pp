5f7232b420e02eaa38c14a7be75034d9b3cdd64b
intrigeri <intrigeri@boum.org>
2015-08-31 10:00:09 +0000

    Add validation for apt::key's name.
    It's great to document requirements in README, but error'ing out whenever
    the user messes up is even better IMO.

--- manifests/key.pp
+++ manifests/key.pp
@@ -1,4 +1,9 @@
 define apt::key ($source, $ensure = 'present') {
+  validate_re(
+    $name, '\.gpg$',
+    'An apt::key resource name must have the .gpg extension',
+  )
+
   file {
     "/etc/apt/trusted.gpg.d/${name}":
       ensure => $ensure,
