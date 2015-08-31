8745de17d64a6eac0eb9f15c19f990fd80383c1f
intrigeri <intrigeri@boum.org>
2015-08-31 09:55:17 +0000

    Quote apt-key variable parameter.
    This is not perfect protection against special chars that the shell may
    interpret, but should help at least in case $name contains spaces.

--- manifests/key/plain.pp
+++ manifests/key/plain.pp
@@ -5,7 +5,7 @@ define apt::key::plain ($source) {
     "${apt::apt_base_dir}/keys":
       ensure  => directory;
   }
-  exec { "apt-key add ${apt::apt_base_dir}/keys/${name}":
+  exec { "apt-key add '${apt::apt_base_dir}/keys/${name}'":
     subscribe   => File["${apt::apt_base_dir}/keys/${name}"],
     refreshonly => true,
     notify      => Exec['refresh_apt'],
