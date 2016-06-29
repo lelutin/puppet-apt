195ed5d31bf5dabd5efe4460127002351499eac2
intrigeri <intrigeri@boum.org>
2016-06-29 17:39:14 +0000

    apt::dist_upgrade: add a timeout parameter (#8).

--- manifests/dist_upgrade.pp
+++ manifests/dist_upgrade.pp
@@ -1,8 +1,13 @@
-class apt::dist_upgrade {
+# Parameters:
+# - timeout: specified in seconds
+class apt::dist_upgrade (
+  $timeout = 300,
+) {
 
   exec { 'apt_dist-upgrade':
     command     => '/usr/bin/apt-get -q -y -o \'DPkg::Options::=--force-confold\' dist-upgrade',
     refreshonly => true,
+    timeout     => $timeout,
     before      => Exec['apt_updated']
   }
 
