70118955f6dae59a021880865408cf9367c3dc57
intrigeri <intrigeri@boum.org>
2016-06-29 17:56:40 +0000

    apt::reboot_required_notify: get rid of temporary variable.

--- manifests/reboot_required_notify.pp
+++ manifests/reboot_required_notify.pp
@@ -1,13 +1,6 @@
 class apt::reboot_required_notify {
 
-  $jessie_or_newer = $::operatingsystemmajrelease ? {
-    5       => false,
-    6       => false,
-    7       => false,
-    default => true,
-  }
-
-  if $jessie_or_newer {
+  if versioncmp($::operatingsystemmajrelease, 8) >= 0 {
     class { 'apt::reboot_required_notify::jessie': }
     # Clean up systems that were upgraded from Wheezy or earlier:
     class { 'apt::reboot_required_notify::wheezy': ensure => absent }
