263c8e96cecb2babd065b66d4c67fb648b0dc079
varac <varacanero@zeromail.org>
2016-11-02 22:47:30 +0100

    Improve tests setup
    - Add .fixtures.yaml to checkout common module
    - Use puppetlabs-spec-helper in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
 # https://puppetlabs.com/blog/testing-modules-in-the-puppet-forge
-require 'rspec-puppet'
-require 'mocha/api'
+require 'puppetlabs_spec_helper/module_spec_helper'
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
 
-  c.module_path = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+  c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.module_path  = File.join(fixture_path, 'modules')
+
   c.color = true
-
-  #Puppet.features.stubs(:root? => true)
-
 end
