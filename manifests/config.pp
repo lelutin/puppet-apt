5e9099b77546a77492402b4003bc9e002bcc4650
Louis-Philippe Véronneau <pollito@riseup.net>
2017-02-24 15:32:15 -0500

    preferences.d/stable is the new preferences

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,7 +3,7 @@ class apt::config {
   exec { 'update_apt':
     command     => '/usr/bin/apt-get update',
     require     => [ File['/etc/apt/apt.conf.d',
-                          '/etc/apt/preferences',
+                          '/etc/apt/preferences.d/stable',
                           '/etc/apt/sources.list'] ],
     refreshonly => true;
   }
